a0e0ec3a842343b7af43cb6c74a9138bc8091050
VoodooSV <voodoosv@gmail.com>
2019-03-13 09:17:59 +0200

    Add isSubsequence

---
+++ 6. 100% OPTIONAL Challenges/04-isSubsequence.js
@@ -0,0 +1,39 @@
+// Write a function called isSubsequence which takes in two strings and checks whether
+// the characters in the first string form a subsequence of the characters in the second
+// string. In other words, the function should check whether the characters in the first
+// string appear somewhere in the second string, without their order changing.
+
+// Examples:
+// isSubsequence('hello', 'by hecatl in lo world') // true
+// isSubsequence('sing', 'sting') // true
+// isSubsequence('abc', 'abracadabra') // true
+// isSubsequence('abc', 'acb') // false
+
+// Your solution MUST have AT LEAST the following complexities:
+// Time Complexity - O(N + M)
+// Space Complexity - O(1)
+
+function isSubsequence(str1, str2) {
+  if (str1.length > str2.length) return false;
+
+  let i = 0;
+  let j = 0;
+  while (j < str2.length) {
+    if (str1[i] === str2[j]) {
+      i++;
+    }
+
+    if (i === str1.length) return true;
+
+    j++;
+}
+
+  return false;
+}
+
+// isSubsequence('hello', 'by hecatl in lo world') // true
+// isSubsequence('sing', 'sting') // true
+// isSubsequence('abc', 'abracadabra') // true
+// isSubsequence('abc', 'acb') // false
+
+console.log(isSubsequence('abc', 'acb'))
