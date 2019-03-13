c060de529c060fdd8230f7d461d5798185e7e088
VoodooSV <voodoosv@gmail.com>
2019-03-13 09:45:05 +0200

    Refactor isSubsequence

--- 6. 100% OPTIONAL Challenges/04-isSubsequence.js
+++ 6. 100% OPTIONAL Challenges/04-isSubsequence.js
@@ -17,16 +17,10 @@ function isSubsequence(str1, str2) {
   if (str1.length > str2.length) return false;
 
   let i = 0;
-  let j = 0;
-  while (j < str2.length) {
-    if (str1[i] === str2[j]) {
-      i++;
-    }
-
+  for (let j = 0; j < str2.length; j++) {
+    if (str1[i] === str2[j]) i++;
     if (i === str1.length) return true;
-
-    j++;
-}
+  }
 
   return false;
 }
@@ -36,4 +30,4 @@ function isSubsequence(str1, str2) {
 // isSubsequence('abc', 'abracadabra') // true
 // isSubsequence('abc', 'acb') // false
 
-console.log(isSubsequence('abc', 'acb'))
+console.log(isSubsequence('hello', 'by hecatl in lo world'))
