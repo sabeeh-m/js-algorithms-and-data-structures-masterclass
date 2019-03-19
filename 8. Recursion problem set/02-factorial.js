3efe6be83ac687cbbd51511f4050d26f1c66dacf
VoodooSV <voodoosv@gmail.com>
2019-03-19 08:54:43 +0200

    Add factorial

---
+++ 8. Recursion problem set/02-factorial.js
@@ -0,0 +1,26 @@
+// Write a function factorial which accepts a number and returns the factorial
+// of that number. A factorial is the product of an integer and all the integers
+// bellow it; e.g., factorial four (4!) is equal to 24, because 4 * 3 * 2 * 1
+// equals 24. factorial zero (0!) is always 1;
+
+// factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
+
+function factorial(num) {
+  if (num < 0) return 0;
+  if (num === 0) return 1;
+
+  return num * factorial(num - 1);
+}
+
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+
+// factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
+
+console.log(factorial(7))
