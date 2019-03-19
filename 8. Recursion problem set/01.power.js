9159e82f4fb8b69f917392e0cb2665168f6d15e8
VoodooSV <voodoosv@gmail.com>
2019-03-19 08:50:12 +0200

    Add power

---
+++ 8. Recursion problem set/01.power.js
@@ -0,0 +1,23 @@
+// Write a function called power which accepts a base and an exponent.
+// The function should return the power of the base to the exponent.
+// This function should mimic the functionality of Math.pow()
+// - do not worry about negative bases and exponents
+
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(base, exp) {
+  if (exp === 0) return 1;
+
+  return base * power(base, exp--);
+}
+
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+console.log(power(2,4))
