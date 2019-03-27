c1dc4c7e2e3923485e39d3005c9b28bd58b4eb4d
VoodooSV <voodoosv@gmail.com>
2019-03-27 09:18:14 +0200

    Add someRecursive

---
+++ 9. Bonus CHALLENGING Recursion Problems/03-someRecursive.js
@@ -0,0 +1,27 @@
+// Write a recursive function called someRecursive which accepts an array and a callback.
+// The function returns true if a single value in the array returns true when passed to the callback.
+// Otherwise it returns false.
+
+// const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+function someRecursive(arr, callback) {
+  if (arr.length === 0) return false;
+
+  if (callback(arr[0])) return true;
+
+  return someRecursive(arr.slice(1), callback);
+}
+
+const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+console.log(someRecursive([4,6,8,10], val => val > 10))
